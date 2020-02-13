f22a7680203da2f1e33312c5d8d9c301c47d8ea5
Yoshitaka Totsuka <yoshitaka.totsuka@siiibo.com>
2020-02-14 04:30:05 +0900

    Normalize

--- src/elm/Color.elm
+++ src/elm/Color.elm
@@ -1,36 +1,87 @@
-module Color exposing (blue, gray, green, red, toneFromInt)
+module Color exposing (blue, gray, green, red)
 
 import Css exposing (Color, hsl)
 
 
-toneFromInt : Int -> Float
-toneFromInt int =
-    case ( int < 0, int > 9 ) of
-        ( True, False ) ->
-            0
-
-        ( False, True ) ->
-            0.9
-
-        _ ->
-            (*) 0.1 <| toFloat int
-
-
 gray : Int -> Color
 gray l =
-    hsl 0 0 (toneFromInt l)
+    hsl 0 0 (normalize l)
 
 
 red : Int -> Color
 red l =
-    hsl 0 1 (toneFromInt l)
+    hsl 0 1 (normalize l)
 
 
 green : Int -> Color
 green l =
-    hsl 120 1 (toneFromInt l)
+    hsl 120 1 (normalize l)
 
 
 blue : Int -> Color
 blue l =
-    hsl 240 1 (toneFromInt l)
+    hsl 240 1 (normalize l)
+
+
+
+-- HELPERS
+
+
+normalize : Int -> Float
+normalize input =
+    input
+        |> highCutOver 9
+        |> lowCutUnder 0
+        |> (\n ->
+                case n of
+                    0 ->
+                        0.95
+
+                    1 ->
+                        0.9
+
+                    2 ->
+                        0.85
+
+                    3 ->
+                        0.8
+
+                    4 ->
+                        0.7
+
+                    5 ->
+                        0.6
+
+                    6 ->
+                        0.5
+
+                    7 ->
+                        0.4
+
+                    8 ->
+                        0.3
+
+                    9 ->
+                        0.2
+
+                    _ ->
+                        0
+           )
+
+
+highCutOver : Int -> Int -> Int
+highCutOver max n =
+    if n > max then
+        max
+
+    else
+        n
+
+
+lowCutUnder : Int -> Int -> Int
+lowCutUnder min n =
+    if n < min then
+        min
+
+    else
+        n
