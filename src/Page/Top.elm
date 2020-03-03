e643b479a36447d2e294f14c915af5403fa0f475
Yoshitaka Totsuka <yoshitaka.totsuka@siiibo.com>
2020-03-03 18:04:06 +0900

    Use y047aka/elm-hsl-color

--- src/Page/Top.elm
+++ src/Page/Top.elm
@@ -1,10 +1,11 @@
 module Page.Top exposing (view)
 
-import Color exposing (..)
 import Css exposing (..)
 import Css.Extra exposing (..)
 import Css.Global exposing (adjacentSiblings)
 import Css.ResetCss
+import HslaColor exposing (..)
+import HslaColor.Manipulate as HslaColor exposing (rotateHue)
 import Html
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (css)
@@ -22,33 +23,33 @@ view =
                 [ -4.5, -4, -3, -2, -1, 0, 1, 2, 3 ]
             , strips red
                 [ -4.5, -4, -3, -2, -1, 0, 1, 2, 3 ]
-            , strips pink
+            , strips (magenta |> rotateHue 30)
                 [ -4.5, -4, -3, -2, -1, 0, 1, 2, 3 ]
             , strips magenta
                 [ -4.5, -4, -3, -2, -1, 0, 1, 2, 3 ]
-            , strips violet
+            , strips (blue |> rotateHue 30)
                 [ -4.5, -4, -3, -2, -1, 0, 1, 2, 3 ]
             , strips blue
                 [ -4.5, -4, -3, -2, -1, 0, 1, 2, 3 ]
-            , strips sky
+            , strips (cyan |> rotateHue 30)
                 [ -4.5, -4, -3, -2, -1, 0, 1, 2, 3 ]
             , strips cyan
                 [ -4.5, -4, -3, -2, -1, 0, 1, 2, 3 ]
-            , strips teal
-                [ -4.5, -4, -3, -2, -1, 0, 1, 2, 3 ]
-            , strips green
+            , strips (lime |> rotateHue 30)
                 [ -4.5, -4, -3, -2, -1, 0, 1, 2, 3 ]
             , strips lime
                 [ -4.5, -4, -3, -2, -1, 0, 1, 2, 3 ]
+            , strips (yellow |> rotateHue 30)
+                [ -4.5, -4, -3, -2, -1, 0, 1, 2, 3 ]
             , strips yellow
                 [ -4.5, -4, -3, -2, -1, 0, 1, 2, 3 ]
-            , strips orange
+            , strips (red |> rotateHue 30)
                 [ -4.5, -4, -3, -2, -1, 0, 1, 2, 3 ]
             ]
         ]
 
 
-strips : HslaSpace -> List Float -> Html msg
+strips : HslaColor -> List Float -> Html msg
 strips hslaSpace array =
     ul
         [ css
@@ -71,7 +72,7 @@ strips hslaSpace array =
                     [ div
                         [ css
                             [ height (px 50)
-                            , backgroundColor (hsla_ <| Color.darken v <| hslaSpace)
+                            , backgroundColor (hsla_ <| HslaColor.darken (v * 0.1) <| hslaSpace)
                             ]
                         ]
                         []
@@ -85,17 +86,24 @@ strips hslaSpace array =
                             , color (hex "#555")
                             ]
                         ]
-                        [ text <|
-                            if v > 0 then
-                                hslaSpace.value ++ " +" ++ String.fromFloat v
-
-                            else if v == 0 then
-                                hslaSpace.value
-
-                            else
-                                hslaSpace.value ++ " " ++ String.fromFloat v
+                        [-- text <|
+                         -- if v > 0 then
+                         --     hslaSpace.value ++ " +" ++ String.fromFloat v
+                         -- else if v == 0 then
+                         --     hslaSpace.value
+                         -- else
+                         --     hslaSpace.value ++ " " ++ String.fromFloat v
                         ]
                     ]
             )
             array
         )
+
+
+hsla_ : HslaColor -> Color
+hsla_ hslaColor =
+    let
+        { hue, saturation, lightness, alpha } =
+            HslaColor.toHsla hslaColor
+    in
+    Css.hsla (hue * 360) saturation lightness alpha
